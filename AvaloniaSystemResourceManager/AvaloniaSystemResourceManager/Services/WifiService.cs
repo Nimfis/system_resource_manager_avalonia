1640c8fc569c1d33d21123cff5fa8a4598f40bd2
Nimfis <anna.rusnak93@gmail.com>
2024-06-29 16:01:32 +0200

    Updated WiFi service

--- AvaloniaSystemResourceManager/AvaloniaSystemResourceManager/Services/WifiService.cs
+++ AvaloniaSystemResourceManager/AvaloniaSystemResourceManager/Services/WifiService.cs
@@ -2,12 +2,17 @@
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.NetworkInformation;
+using System.Timers;
 
 namespace AvaloniaSystemResourceManager.Services
 {
     internal class WifiService
     {
         private NetworkInterface _wifiInterface;
+        private IPv4InterfaceStatistics _previousStats;
+        private Timer _updateTimer;
+        private double _lastSentMBPerSecond;
+        private double _lastReceivedMBPerSecond;
 
         public WifiService()
         {
@@ -16,6 +21,30 @@ namespace AvaloniaSystemResourceManager.Services
             {
                 throw new InvalidOperationException("No Wi-Fi network interface found.");
             }
+
+            UpdateCurrentStats(); // Initial update
+            _updateTimer = new Timer(1000); // Set the timer to update every second
+            _updateTimer.Elapsed += UpdateUsagePerSecond;
+            _updateTimer.Start();
+        }
+
+        private void UpdateUsagePerSecond(object sender, ElapsedEventArgs e)
+        {
+            var currentStats = _wifiInterface.GetIPv4Statistics();
+            if (_previousStats != null)
+            {
+                var sentBytes = currentStats.BytesSent - _previousStats.BytesSent;
+                var receivedBytes = currentStats.BytesReceived - _previousStats.BytesReceived;
+
+                _lastSentMBPerSecond = sentBytes / (1024.0 * 1024.0);
+                _lastReceivedMBPerSecond = receivedBytes / (1024.0 * 1024.0);
+            }
+            _previousStats = currentStats; // Update the previous stats for the next calculation
+        }
+
+        public Task<(double SentMBPerSecond, double ReceivedMBPerSecond)> GetCurrentWifiUsagePerSecondAsync()
+        {
+            return Task.FromResult((_lastSentMBPerSecond, _lastReceivedMBPerSecond));
         }
 
         private NetworkInterface GetWifiInterface()
@@ -25,15 +54,9 @@ namespace AvaloniaSystemResourceManager.Services
                                                          ni.OperationalStatus == OperationalStatus.Up);
         }
 
-        public async Task<(double SentMB, double ReceivedMB)> GetWifiUsageAsync()
+        private void UpdateCurrentStats()
         {
-            return await Task.Run(() =>
-            {
-                var stats = _wifiInterface.GetIPv4Statistics();
-                double sentMB = stats.BytesSent / (1024.0 * 1024.0);
-                double receivedMB = stats.BytesReceived / (1024.0 * 1024.0);
-                return (sentMB, receivedMB);
-            });
+            _previousStats = _wifiInterface.GetIPv4Statistics();
         }
 
         public async Task<string> GetWifiSSIDAsync()
